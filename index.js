b76eaf4d22f2bca03ac6262f573782c7009b4b31
leidy822 <leidyc2846@gmail.com>
2022-11-27 16:22:26 -0600

    while+function

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 
-alert("Bienvenido a tu simulador de cuotas para vivienda propia");
-let nombre= prompt("Cual es tu nombre?");
-let PagoInicial= parseInt(prompt("Cuanto Considerarias darnos para Pago Inicial o Downpayment?"));
+    alert("Bienvenido a tu simulador de cuotas para vivienda propia");
+    let nombre = prompt("Cual es tu nombre?");
+    let valorHome = parseInt(prompt("Cual es valor de la propiedad que deseas obtener?"));
+    let anos = parseInt(prompt(`${nombre} en cuantos años deseas finalizar tu deuda?`));
+    let interes = "0.10";
+    let descuento = " 0.15";
 
-if (PagoInicial <= 1000) {
-    let opcion1 = (`${nombre} Ingresaste ${PagoInicial} Lamentablemente no podemos projectar cuotas ya que es muy bajo el pago inicial`);
-    alert(opcion1);
-} else if (PagoInicial <= 2000) {
-    let opcion2 = (`${nombre} Ingresaste ${PagoInicial} Super ...! para ello intentaremos ofrecerte los menores interes`);
-    alert(opcion2);
-}else if (PagoInicial <= 3000) {
-    let opcion3 = (`${nombre} Ingresaste ${PagoInicial} Perfecto ...! contamos con opciones para ti, Tenemos los menores intereses`);
-    alert(opcion3);
-}else if (PagoInicial <= 4000) {
-    let opcion4 = (`${nombre} Ingresaste ${PagoInicial} Excelente...! Contamos con los mas bajos intereses del mercado para ti`);
-    alert(opcion4);
-}else if (PagoInicial <= 8000 ) {
-    let opcion5 = (`${nombre} Ingresaste ${PagoInicial} Estamos muy contentos por ti...! Contamos con los mas bajos intereses del mercado para ti + un descuento adicional`);
-    alert(opcion5);
-}else {
-    alert("No Aplicamos valores tan altos para un Downpayment, Intenta nuevamente");
-}
+    let pagoInicial = parseInt(prompt("Cuanto Considerarias darnos para Pago Inicial o Downpayment?"));
+    if (pagoInicial <= 1000) {
+        let opcion1 = (`${nombre} Ingresaste ${pagoInicial} Lamentablemente no podemos projectar cuotas ya que es muy bajo el pago inicial`);
+        alert(opcion1);
+    } else if (pagoInicial <= 2000) {
+        let opcion2 = (`${nombre} Ingresaste ${pagoInicial} Super ...! para ello intentaremos ofrecerte los menores interes`);
+        alert(opcion2);
+    } else if (pagoInicial <= 3000) {
+        let opcion3 = (`${nombre} Ingresaste ${pagoInicial} Perfecto ...! contamos con opciones para ti, Tenemos los menores intereses`);
+        alert(opcion3);
+    } else if (pagoInicial <= 4000) {
+        let opcion4 = (`${nombre} Ingresaste ${pagoInicial} Excelente...! Contamos con los mas bajos intereses del mercado para ti`);
+        alert(opcion4);
+    } else if (pagoInicial <= 8000) {
+        let opcion5 = (`${nombre} Ingresaste ${pagoInicial} Estamos muy contentos por ti...!Contamos con los mas bajos intereses del mercado para ti + un descuento adicional`);
+        alert(opcion5);
+    } else {
+        alert("No se Aplica estos valores tan altos para un Downpayment, Intenta nuevamente");
+    }
 
+    function f1(valorHome, pagoInicial, interes, anos,) {
+        let resultado = (valorHome - pagoInicial * (interes) / (anos));
+        let mensaje = (`${nombre} Gracias por aplicar , con tu informacion recolectada y la cantidad de años tus pagos mensuales serian de ${resultado}`);
+        alert(mensaje);
+    }
+
+while (pagoInicial <= 8000) {
+    let mensaje2 = "Tienes un descuento adicional del 15% del valor de la propiedad";
+    alert(mensaje2);
+    }
